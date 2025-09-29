b6e37d57f26c7d36a9ead1324fca8caba6657048
Marcusicoi <72920116+Marcusicoi@users.noreply.github.com>
2025-09-29 23:01:29 +0800

    Add files via upload

---
+++ florr/tmr/builds/6-18-24/set.js
@@ -0,0 +1,27 @@
+let news = [
+  "Primitive", "Generic", "Pecuilar", "Limited",
+  "Exquisite", "Elite", "Arcane", "Outré", "Sublime",
+  "Supreme", "Obescuration", "Astral", "Transcendent",
+  "Stellar", "Quantum", "Malevolent", "Cataclysmic",
+  "Infernal", "Hel", "Abyssal", "Radiant", "Argenic",
+  "Opulent", "Genesis", "Aleph", "Awakened", "Preeminent",
+  "Transcendentem",
+];
+let changes = [];
+let removed = [];
+let creds = {
+  Centxide: ["Allegorical", "Resplendent", "Clandestine", "Venerable", "Magnificent", "Dazzling", "Hermetic", "Archetypal"],
+  Demoflower: ["Valve"],
+  Floof: ["Ancient", "Omega", "???"],
+  Flowr: ["Astral", "Transcendent", "Vicious", "Quantum", "Supreme"],
+  Kosuken: ["Cataclysmic", "Abyssal"],
+  "Lit/-": ["Whimsical"],
+  Mysteryflower: ["Radiant"],
+  NullArea: ["Perfect"],
+  TechCourse: ["Aleph"],
+  cheese: ["Opulent", "Genesis", "Argenic", "Outrè", "Peculiar", "Primitive", "Generic", "Stellar", "Sublime", "Superlative", "Exquisite", "Limited"],
+  iceyusuper: ["Elite", "Awakened", "Obescuration", "Infernal"],
+  shiryuu_1234: ["Useless"],
+  smallw: ["Absurd", "Ridiculous", "Limitless"],
+  ".thedragonfish": ["Ubiquitous"],
+};
