076472fd4ccba766272a6a4efcf0545621c1999b
Marcusicoi <72920116+Marcusicoi@users.noreply.github.com>
2025-09-29 22:59:42 +0800

    Add files via upload

---
+++ florr/tmr/builds/11-23-24/set.js
@@ -0,0 +1,30 @@
+let news = ["Seraphic", "Vicious", "Galactic",
+  "Tremendous", "Luminescent", "Titanic",
+  "Luminous", "Sovereign", "Primordial",
+  "Elysian", "Paramount", "Sacred", "Ruthless",
+  "Monocosm", "Secode", "Esoteric", "Cosmical",
+  "Deific", "Recherché"
+];
+let changes = [];
+let removed = [];
+let creds = {
+  Centxide: ["Allegorical", "Resplendent", "Clandestine", "Venerable", "Magnificent", "Dazzling", "Hermetic", "Archetypal"],
+  Demoflower: ["Valve"],
+  Floof: ["Ancient", "Omega", "???"],
+  Flowr: ["Astral", "Seraphic", "Galactic", "Transcendent", "Vicious", "Quantum", "Supreme"],
+  GiraR487: ["Esoteric"],
+  Harry: ["Sacred"],
+  Kosuken: ["Cataclysmic", "Abyssal"],
+  "Lit/-": ["Whimsical"],
+  Moonshade: ["Luminous", "Elysian", "Ruthless"],
+  Mysteryflower: ["Radiant"],
+  NullArea: ["Tremendous", "Perfect"],
+  TechCourse: ["Aleph"],
+  cheese: ["Opulent", "Genesis", "Argenic", "Outrè", "Peculiar", "Primitive", "Generic", "Stellar", "Sublime", "Superlative", "Exquisite", "Limited"],
+  iceyusuper: ["Elite", "Awakened", "Obescuration", "Infernal"],
+  lunoven: ["Sovereign", "Primordial", "Paramount"],
+  paddymelon: ["Luminescent"],
+  shiryuu_1234: ["Useless"],
+  smallw: ["Absurd", "Ridiculous", "Limitless"],
+  ".thedragonfish": ["Ubiquitous"],
+};
