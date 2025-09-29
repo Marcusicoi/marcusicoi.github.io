0213343580bbf2ff624b53ae0630a7f4d6fb81d7
Marcusicoi <72920116+Marcusicoi@users.noreply.github.com>
2025-09-29 23:01:59 +0800

    Add files via upload

---
+++ florr/tmr/builds/9-21-23/set.js
@@ -0,0 +1,12 @@
+let news = [
+  "Ethereal"
+];
+let changes = [1, ["Hyper", "Hyperbolic", "Terrestrial", "Celestial"]];
+let removed = [];
+let creds = {
+  Centxide: ["Allegorical", "Resplendent", "Clandestine", "Venerable", "Magnificent", "Dazzling", "Hermetic", "Archetypal"],
+  Demoflower: ["Valve"],
+  Floof: ["Ancient", "Omega", "???"],
+  shiryuu_1234: ["Useless"],
+  smallw: ["Absurd", "Ridiculous", "Limitless"],
+};
