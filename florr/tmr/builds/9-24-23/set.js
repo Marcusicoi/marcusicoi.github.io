66000b5d61f6908500ee26b088873bef8f5b193e
Marcusicoi <72920116+Marcusicoi@users.noreply.github.com>
2025-09-29 23:02:29 +0800

    Add files via upload

---
+++ florr/tmr/builds/9-24-23/set.js
@@ -0,0 +1,15 @@
+let news = [
+  "Ubiquitous", "Omnipresent", "Mediocre", "Precious",
+  "Multiversal", "Megaversal", "Gigaversal", "Hyperversal",
+  "Omniversal", "Outerversal",
+];
+let changes = [];
+let removed = [];
+let creds = {
+  Centxide: ["Allegorical", "Resplendent", "Clandestine", "Venerable", "Magnificent", "Dazzling", "Hermetic", "Archetypal"],
+  Demoflower: ["Valve"],
+  Floof: ["Ancient", "Omega", "???"],
+  shiryuu_1234: ["Useless"],
+  smallw: ["Absurd", "Ridiculous", "Limitless"],
+  ".thedragonfish": ["Ubiquitous"],
+};
