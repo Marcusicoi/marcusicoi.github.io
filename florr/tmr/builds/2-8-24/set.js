8e3201b133150e8ed6aa8c3124cec09d8df57a16
Marcusicoi <72920116+Marcusicoi@users.noreply.github.com>
2025-09-29 23:00:25 +0800

    Add files via upload

---
+++ florr/tmr/builds/2-8-24/set.js
@@ -0,0 +1,16 @@
+let news = [
+  "Superlative", "Perfect", "Whimsical"
+];
+let changes = [];
+let removed = [];
+let creds = {
+  Centxide: ["Allegorical", "Resplendent", "Clandestine", "Venerable", "Magnificent", "Dazzling", "Hermetic", "Archetypal"],
+  Demoflower: ["Valve"],
+  Floof: ["Ancient", "Omega", "???"],
+  "Lit/-": ["Whimsical"],
+  NullArea: ["Perfect"],
+  cheese: ["Superlative"],
+  shiryuu_1234: ["Useless"],
+  smallw: ["Absurd", "Ridiculous", "Limitless"],
+  ".thedragonfish": ["Ubiquitous"],
+};
